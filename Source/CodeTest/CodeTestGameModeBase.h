19f9989217d1ebc6225bc1d7131c05fa87ee14da
DESKTOP-L4LLM8G\60294 <602941661@qq.com>
2019-10-05 18:05:48 +0800

    Initial commit

---
+++ Source/CodeTest/CodeTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CodeTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODETEST_API ACodeTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
